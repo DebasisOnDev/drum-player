e1dc765cab8a3e09acebc9c090b1bf05b4915577
DebasisOnDev <132636140+DebasisOnDev@users.noreply.github.com>
2023-07-02 08:43:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+const box4 = document.querySelector(".box4");
+const aud = document.querySelector(".audio");
+box1.addEventListener("click", () => {
+  aud.play();
+});
+box2.addEventListener("click", () => {
+  aud.play();
+});
+box3.addEventListener("click", () => {
+  aud.play();
+});
+box4.addEventListener("click", () => {
+  aud.play();
+});
